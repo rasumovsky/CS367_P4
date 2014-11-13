c6a5dc887850d1b70a97411cec48adfeda23fef2
Andrew Hard <hardandrew1@gmail.com>
2014-11-13 12:32:06 -0600

    Initial check-in

---
+++ GuessingGame.java
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// Title:            GuessingGame
+// Files:            GuessingGame.java, BinaryTree.java, BinaryTreenode.java
+//                   IllegalBinaryOpException.java
+// Semester:         CS302 Fall 2014
+//
+// Author:           Andrew Hard
+// Email:            hard@wisc.edu
+// CS Login:         hard
+// Lecturer's Name:  Jim Skrentny
+// Lab Section:      LEC-002 (77632)
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+// Pair Partner:     Wayne Chew
+// Email:            mchew2@wisc.edu
+// CS Login:         mchew
+// Lecturer's Name:  Jim Skrentny
+// Lab Section:      LEC-001 (77631)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+import java.io.*;
---
+++ README.txt
@@ -0,0 +1,12 @@
+================================================
+      README file for Programming Pairs
+================================================
+
+Programming Assignment Number: 4
+Date Completed: 11/25/2014
+
+Partner 1     Name: Wayne Chew
+Partner 1 CS Login: mchew
+
+Partner 2     Name: Andrew Hard
+Partner 2 CS Login: hard
