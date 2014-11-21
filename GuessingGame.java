703f79500845227805a9715cf8510ac98870b916
xpheal <xpheal@gmail.com>
2014-11-21 02:31:15 -0600

    Working on GuessingGame

--- GuessingGame.java
+++ GuessingGame.java
@@ -31,8 +31,34 @@ import java.io.*;
  * @author Andrew Hard
  */
 public class GuessingGame{
+	public static void main(String[] args) throws IllegalBinaryTreeOpException, FileNotFoundException{
+		if(args.length != 1){
+			System.out.println("Usage: java GuessingGame Game_File_Name");
+			System.exit(0);
+		}
 
-	public static void main(String[] args) throws IllegalBinaryTreeOpException{
+		File gameFile = new File(args[0]);
+		if(!gameFile.exists()){
+			System.out.println("Cannot find the specified file");
+		}
+
+		Scanner gameInput = new Scanner(gameFile);
+
+		List<String> commands = new ArrayList<String>();
+		String command;
+		while(gameInput.hasNext()){
+			//commands.add(gameInput.next());
+			command = gameInput.nextLine();
+
+			switch(command){
+
+			}
+		}
+
+		/*for(int i=0; i<command.size(); i++){
+			System.out.println(command.get(i));
+		}*/
+
 		BinaryTree<String> game = new BinaryTree<String>("Hello");
 		game.start();
 		game.addRightChild("YOLO");
