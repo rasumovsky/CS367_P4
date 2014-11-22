ed11d89a34fadf8521ba0b531ef01209213b34b5
xpheal <xpheal@gmail.com>
2014-11-22 16:08:41 -0600

    done without checking for bugs and exceptions

--- GuessingGame.java
+++ GuessingGame.java
@@ -31,40 +31,147 @@ import java.io.*;
  * @author Andrew Hard
  */
 public class GuessingGame{
-	public static void main(String[] args) throws IllegalBinaryTreeOpException, FileNotFoundException{
-		if(args.length != 1){
-			System.out.println("Usage: java GuessingGame Game_File_Name");
-			System.exit(0);
-		}
 
-		File gameFile = new File(args[0]);
-		if(!gameFile.exists()){
-			System.out.println("Cannot find the specified file");
-		}
+	public static BinaryTree<String> reset(Scanner in) throws IllegalBinaryTreeOpException{
+		BinaryTree<String> root;
+		
+		System.out.println("Please enter a question.");
+		root = new BinaryTree<String>(in.nextLine());
+		root.start();
 
-		Scanner gameInput = new Scanner(gameFile);
+		System.out.println("Please enter something that is true for that question.");
+		root.addLeftChild(in.nextLine());
 
-		List<String> commands = new ArrayList<String>();
+		System.out.println("Please enter something that is false for that question.");
+		root.addRightChild(in.nextLine());
+
+		return root;
+
+	}
+
+	public static void play(Scanner in, BinaryTree<String> root) throws IllegalBinaryTreeOpException{
 		String command;
-		while(gameInput.hasNext()){
-			//commands.add(gameInput.next());
-			command = gameInput.nextLine();
+		root.start();
 
-			switch(command){
+		while(!root.isLeaf()){
+			System.out.println(root.getCurrent());
+			command = in.nextLine().toLowerCase();
 
+			if(command.equals("y")){
+				root.goLeft();
+			}
+			else if(command.equals("n")){
+				root.goRight();
+			}
+			else{
+				System.out.println("Invalid Command");
 			}
 		}
 
-		/*for(int i=0; i<command.size(); i++){
-			System.out.println(command.get(i));
-		}*/
+		String oldGuess = root.getCurrent();
+		System.out.println("I guess: " + oldGuess + ". Was I right?");
 
-		BinaryTree<String> game = new BinaryTree<String>("Hello");
-		game.start();
-		game.addRightChild("YOLO");
+		command = in.nextLine().toLowerCase();
 
-		game.addLeftChild("Muahaha");
+		if(command.equals("y")){
+			System.out.println("I win!");
+		}
+		else if(command.equals("n")){
+			System.out.println("Darn. Ok, tell me a question that is true for your answer, but false for my guess.");
+			root.changeCurrent(in.nextLine());
+			System.out.println("Thanks! And what were you thinking of?");
+			root.addLeftChild(in.nextLine());
+			root.addRightChild(oldGuess);
+		}
+		else{
+			System.out.println("Invalid Command");
+		}
+	}
 
-		game.print();
+	public static void main(String[] args) throws IllegalBinaryTreeOpException, FileNotFoundException{
+		File gameFile;
+		String command;
+		BinaryTree<String> root = null;
+
+		if(args.length != 1 && args.length != 0){
+			System.out.println("Usage: java GuessingGame Game_File_Name");
+			System.exit(0);
+		}
+		else if(args.length == 1){
+			gameFile = new File(args[0]);
+
+			if(gameFile.exists()){
+				Scanner fileInput = new Scanner(gameFile);
+
+				while(fileInput.hasNext()){
+					System.out.println("Please enter command (o,p,q,r): ");
+					command = fileInput.nextLine();
+					
+					switch(command){
+						case "o" :
+							if(root == null){
+								System.out.println("Empty Tree");
+							}
+							else{
+								root.print();
+							}
+							break;
+						case "p" :
+							if(root == null){
+								root = reset(fileInput);
+							}
+							play(fileInput, root);
+							break;
+						case "q" :
+							fileInput.close();
+							System.exit(0);
+							break;
+						case "r" :
+							root = reset(fileInput);
+							break;
+						default:
+							break;
+					}
+				}
+				fileInput.close();
+			}
+		}
+
+
+		Scanner userInput = new Scanner(System.in);
+
+		while(true){
+			System.out.print("Please enter command (o,p,q,r): ");
+
+			command = userInput.nextLine();
+
+			switch(command){
+				case "o" :
+					if(root == null){
+						System.out.println("Empty Tree");
+					}
+					else{
+						root.print();
+					}
+					break;
+				case "p" :
+					if(root == null){
+						root = reset(userInput);
+					}
+					play(userInput, root);
+
+					break;
+				case "q" :
+					userInput.close();
+					System.exit(0);
+					break;
+				case "r" :
+					root = reset(userInput);
+					break;
+				default:
+					System.out.println("Invalid command.");
+					break;
+			}
+		}
 	}
 }
