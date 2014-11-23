4ced7cf59e78c6dba4facd8b532d443cfa106fe8
Andrew Hard <hardandrew1@gmail.com>
2014-11-23 17:05:44 -0600

    Fixing some Exceptions, adding comments

--- GuessingGame.java
+++ GuessingGame.java
@@ -36,28 +36,41 @@ import java.io.*;
  */
 public class GuessingGame {
     
+    /**
+     * reset is a method for resetting the game. It starts with a completely
+     * empty tree and then prompts the user for a new root for the tree.
+     * @param scanner - the input method (user console input or file).
+     * @returns a new BinaryTree with which the game can restart.
+     * @throws IllegalBinaryTreeOpException.
+     */
     public static BinaryTree<String> reset(Scanner in) 
 	throws IllegalBinaryTreeOpException {
-	
-	BinaryTree<String> root;
-	
+		
+	// Create a new tree root using the question given by user:
 	System.out.println("Please enter a question.");
-	root = new BinaryTree<String>(in.nextLine());
-	root.start();
+	BinaryTree<String> root = new BinaryTree<String>(in.nextLine());
+	root.start();//start navigation of tree by setting current node to root
 	
-	System.out.println("Please enter something that is true for that question.");
+	// Add true answer for the left child:
+	System.out
+	    .println("Please enter something that is true for that question.");
 	root.addLeftChild(in.nextLine());
 	
-	System.out.println("Please enter something that is false for that question.");
+	// Add false answer for the right child:
+	System.out
+	    .println("Please enter something that is false for that question.");
 	root.addRightChild(in.nextLine());
 	
+	// return the root of the new BinaryTree.
 	return root;
-	
     }
     
     
     /**
-     * 
+     * play is a method for plaing the game with the program, based on inputs. 
+     * @param in - the Scanner input from the user or from an input game file.
+     * @param root - the BinaryTree storing game data.
+     * @throws IllegalBinaryTreeOptException.
      */
     public static void play(Scanner in, BinaryTree<String> root) 
 	throws IllegalBinaryTreeOpException {
@@ -65,8 +78,11 @@ public class GuessingGame {
 	String command;
 	root.start();
 	
+	// Continue asking questions until an answer (leaf) is reached in tree:
 	while (!root.isLeaf()) {
+	    // print current question:
 	    System.out.println(root.getCurrent());
+	    // get answer (y/n) from user or file:
 	    command = in.nextLine().toLowerCase();
 	    
 	    if (command.equals("y")) {
@@ -80,6 +96,7 @@ public class GuessingGame {
 	    }
 	}
 	
+	// An answer (leaf node) has been reached:
 	String oldGuess = root.getCurrent();
 	System.out.println("I guess: " + oldGuess + ". Was I right?");
 	
@@ -89,10 +106,14 @@ public class GuessingGame {
 	    System.out.println("I win!");
 	}
 	else if (command.equals("n")) {
+	    // Change current node (a guess) to a new distinguishing question
 	    System.out.println("Darn. Ok, tell me a question that is true for your answer, but false for my guess.");
 	    root.changeCurrent(in.nextLine());
+	    
 	    System.out.println("Thanks! And what were you thinking of?");
+	    // Set left child of current node (question) to user's true answer:
 	    root.addLeftChild(in.nextLine());
+	    // Set right child of current node (question) to false old guess:
 	    root.addRightChild(oldGuess);
 	}
 	else {
@@ -109,8 +130,9 @@ public class GuessingGame {
      * @param input - the Scanner used to input commands.
      * @param command - the current command to parse.
      * @returns true if the user chooses to quit, or if input file ends.
+     * @throws IllegalBinaryTreeOpException.
      */
-    public static boolean checkOptions(BinaryTree root, Scanner input, 
+    public static boolean checkOptions(BinaryTree<String> root, Scanner input, 
 				       String command)
 	throws IllegalBinaryTreeOpException {
 	// true if user chooses to quit
